3c341b93953a6ab8aa2eb0d39ecc8a029c2885ed
Dustin Zeisler <dustin@zive.me>
2013-08-01 12:03:06 -0700

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('mathPlotApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
